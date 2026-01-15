f110b592a2be8f45c5c4d7faff8c4b4762452d76
[dyad] <junior.8350i@gmail.com>
2026-01-15 09:21:36 -0300

    [dyad] Remover texto MadeWithDyad - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,7 +1,5 @@
 export const MadeWithDyad = () => {
   return (
-    <div className="p-4 text-center">
-      <span className="text-sm text-gray-500 dark:text-gray-400">Made with Dyad</span>
-    </div>
+    <div className="p-4 text-center"></div>
   );
 };
